b9004510b6df959fcd6e8a1b9411eda71b52071c
skarthick <sunilkarthicksivabalan@Sunilkarthicks-MacBook-Pro.local>
2017-07-07 16:04:49 +0530

    Bug fix

--- codecs/PLTIT.ts
+++ codecs/PLTIT.ts
@@ -21,14 +21,14 @@ export function decodeSentence(fields: string[]): PLTITPacket {
   return {
     sentenceId: sentenceId,
     sentenceName: sentenceName,
-    horizontalVector: fields[0],
-    horizontalDistance: fields[1],
-    horizontalDistanceUnits: fields[2][0],
-    horizontalAngle: fields[3],
-    horizontalAngleUnits: fields[4][0],
-    verticalAngle: fields[5],
-    verticalAngleUnits: fields[6][0],
-    slopeDistance: fields[7],
-    slopeDistanceUnits: fields[8][0]
+    horizontalVector: fields[1],
+    horizontalDistance: fields[2],
+    horizontalDistanceUnits: fields[3][0],
+    horizontalAngle: fields[4],
+    horizontalAngleUnits: fields[5][0],
+    verticalAngle: fields[6],
+    verticalAngleUnits: fields[7][0],
+    slopeDistance: fields[8],
+    slopeDistanceUnits: fields[9][0]
   };
 };
--- dist/codecs/PTNLB.js
+++ dist/codecs/PTNLB.js
@@ -6,15 +6,15 @@ function decodeSentence(fields) {
     return {
         sentenceId: exports.sentenceId,
         sentenceName: exports.sentenceName,
-        horizontalVector: fields[0],
-        horizontalDistance: fields[1],
-        horizontalDistanceUnits: fields[2][0],
-        horizontalAngle: fields[3],
-        horizontalAngleUnits: fields[4][0],
-        verticalAngle: fields[5],
-        verticalAngleUnits: fields[6][0],
-        slopeDistance: fields[7],
-        slopeDistanceUnits: fields[8][0]
+        horizontalVector: fields[1],
+        horizontalDistance: fields[2],
+        horizontalDistanceUnits: fields[3][0],
+        horizontalAngle: fields[4],
+        horizontalAngleUnits: fields[5][0],
+        verticalAngle: fields[6],
+        verticalAngleUnits: fields[7][0],
+        slopeDistance: fields[8],
+        slopeDistanceUnits: fields[9][0]
     };
 }
 exports.decodeSentence = decodeSentence;
